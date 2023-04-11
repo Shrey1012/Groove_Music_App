877d6589d61e16fe98d2c32e173e29159c3c7ade
Shrey1012 <shrey9157@gmail.com>
2023-04-11 23:54:58 +0530

    Added Loader and Error Component

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,6 +6,10 @@ const Discover = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'Pop';
 
+  if (isFetching) return <Loader title="Loading songs..." />;
+
+  if (error) return <Error />;
+
   return (
     <div className="flex-flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -24,7 +28,7 @@ const Discover = () => {
           ))}
         </select>
         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-          {data?.map((song, i) => (
+          {data?.tracks?.map((song, i) => (
             <SongCard key={song.key} song={song} i={i} />
           ))}
         </div>
